8b1d7b2cfa053676824e7f684dc4803c42bbf156
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-01-31 12:54:13 +0530

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ import java.util.logging.*;
 
 class Basiccreditcard implements Cloneable{
     String name;
-    int creditcardNumber;
+    long creditcardNumber;
     String expirationDate;
-    public Basiccreditcard(String name, int creditcardNumber, String expirationDate){
+    public Basiccreditcard(String name, long creditcardNumber, String expirationDate){
         this.name=name;
         this.creditcardNumber=creditcardNumber;
         this.expirationDate=expirationDate;
@@ -20,7 +20,7 @@ class Basiccreditcard implements Cloneable{
         }
         return "false";
     }
-    public void compare(int credit){
+    public void compare(long credit){
         Logger l = Logger.getLogger("com.api.jar");
         if(creditcardNumber==credit){
             l.info("Both are same");
@@ -32,27 +32,22 @@ class Basiccreditcard implements Cloneable{
     public static void main(String[] args) throws CloneNotSupportedException{
         Logger l = Logger.getLogger("com.api.jar");
         Scanner sc=new Scanner(System.in);
-        try {
-            l.info("name:");
-            String naMe = sc.nextLine();
-            l.info("credit card number:");
-            int creditcardnumber = sc.nextInt();
-            sc.nextLine();
-            l.info("Expiry Date:");
-            String expirationdate = sc.nextLine();
-            l.info("Enter the new person credit card number:");
-            int c1 = sc.nextInt();
-            l.info("checking whether two credit card numbers same or not");
-            Basiccreditcard ba = new Basiccreditcard(naMe, creditcardnumber, expirationdate);
-            ba.compare(c1);
-            l.info("After cloning");
-            Basiccreditcard ba1 = (Basiccreditcard) ba.clone();
-            String k = ba.equally(ba1);
-            l.info(k);
-            sc.close();
-        }
-        catch(InputMismatchException e){
-            l.info("invalid input "+e);
-        }
+        l.info("name:");
+        String naMe = sc.nextLine();
+        l.info("credit card number:");
+        long creditcardnumber = sc.nextLong();
+        sc.nextLine();
+        l.info("Expiry Date:");
+        String expirationdate = sc.nextLine();
+        l.info("Enter the new person credit card number:");
+        long c1 = sc.nextLong();
+        l.info("checking whether two credit card numbers same or not");
+        Basiccreditcard ba = new Basiccreditcard(naMe, creditcardnumber, expirationdate);
+        ba.compare(c1);
+        l.info("After cloning");
+        Basiccreditcard ba1 = (Basiccreditcard) ba.clone();
+        String k = ba.equally(ba1);
+        l.info(k);
+        sc.close();
     }
 }
